79e69e1d1ca8c065340955ec7b7052d281cf450e
Ryan Ball <rball@balldevelopment.com>
2018-01-01 17:07:07 -0700

    s3 file skeleton

--- file/s3/read.go
+++ file/s3/read.go
@@ -1,1 +1,36 @@
 package s3
+
+import (
+	"github.com/pcelvng/task-tools/file/stat"
+)
+
+func NewReader(pth string, opt *Options) *Reader {
+	sts := stat.New()
+	sts.SetPath(pth)
+
+	return &Reader{
+		sts: sts,
+		opt: opt,
+	}
+}
+
+type Reader struct {
+	sts stat.Stat
+	opt *Options
+}
+
+func (w *Reader) ReadLine() (ln []byte, err error) {
+	return
+}
+
+func (w *Reader) Read(p []byte) (n int, err error) {
+	return
+}
+
+func (w *Reader) Stats() stat.Stat {
+	return w.sts.Clone()
+}
+
+func (w *Reader) Close() (err error) {
+	return
+}
