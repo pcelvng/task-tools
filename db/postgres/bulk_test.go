51daf6fe3c4e6f21a72dbcd180435d009a3a858f
Ryan Ball <ryan.ball@rakuten.com>
2018-02-13 14:48:55 -0700

    some postgres db tests

--- db/postgres/bulk_test.go
+++ db/postgres/bulk_test.go
@@ -12,11 +12,11 @@ import (
 )
 
 var pgConnStr = "user=%s password=%s host=%s dbname=%s sslmode=disable"
+var pgConn *sql.DB
 
 func TestMain(m *testing.M) {
 	// postgres user (default is current user)
 	usr, _ := user.Current()
-	log.Println(usr.Username)
 
 	// setup postgres test db
 	pgDB, err := sql.Open("postgres", fmt.Sprintf(pgConnStr, usr.Username, "", "", "postgres"))
@@ -24,6 +24,12 @@ func TestMain(m *testing.M) {
 		log.Fatalln(err)
 	}
 	_, err = pgDB.Exec("CREATE DATABASE ci_test;")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// pg conn for all tests to use
+	pgConn, err = sql.Open("postgres", fmt.Sprintf(pgConnStr, usr.Username, "", "", "ci_test"))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -38,4 +44,18 @@ func TestMain(m *testing.M) {
 	}
 
 	os.Exit(code)
+}
+
+func TestBatchLoader_AddRow(t *testing.T) {
+	bl := NewBatchLoader(pgConn)
+
+	bl.AddRow([]interface{}{"one", "two", "three"})
+	bl.AddRow([]interface{}{"one", "two", "three"})
+	bl.AddRow([]interface{}{"one", "two", "three"})
+
+	got := len(bl.fRows)
+	expected := 9
+	if got != expected {
+		t.Errorf("got %v but expected %v", got, expected)
+	}
 }
