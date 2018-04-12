6172a9a7db515b4290d72c9430d04edf3c3a3e6a
Jeremiah Zink <jeremiah.zink@rakuten.com>
2018-04-12 14:55:49 -0600

    updated watcher to send new files if the size or created date changes, updated to not send a file with a zero size or empty created date

--- apps/utils/filewatcher/watcher_test.go
+++ apps/utils/filewatcher/watcher_test.go
@@ -4,10 +4,12 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"syscall"
 	"testing"
 	"time"
+
+	"github.com/pcelvng/task-tools/file/stat"
+	"github.com/pcelvng/task/bus"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
@@ -17,69 +19,139 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestRun(t *testing.T) {
-	os.Args = append(os.Args, "-config=test.toml")
-	createTomlFile("test.toml")
-	now := time.Now()
-	path := now.Format("2006/01/02/15/")
-	createTestWatchFile(path, "test.file.txt")
+func TestProducerErr(t *testing.T) {
+	o := &options{}
+	o.Options = &bus.Options{}
+	o.Bus = "nop"
+	o.NopMock = "init_err"
+	o.Rules = append(o.Rules, &Rule{})
 
+	_, err := newWatchers(o)
+	assert.NotNil(t, err)
+}
+
+func TestNewWatchers(t *testing.T) {
+	o := &options{}
+	o.Options = &bus.Options{}
+	o.Bus = "nop"
+	o.NopMock = "msg_msg_done"
+	o.Rules = append(o.Rules, &Rule{}, &Rule{})
+
+	ws, _ := newWatchers(o)
+	assert.Equal(t, 2, len(ws))
+}
+
+func TestErrOnStop(t *testing.T) {
+	o := &options{}
+	o.Options = &bus.Options{}
+	o.Bus = "nop"
+	o.NopMock = "stop_err"
+	o.Rules = append(o.Rules, &Rule{}, &Rule{})
+
+	ws1, _ := newWatchers(o)
+	err := closeWatchers(ws1)
+	assert.NotNil(t, err)
+
+	o.NopMock = "msg_msg_done"
+	ws2, _ := newWatchers(o)
+	err = closeWatchers(ws2)
+	assert.Nil(t, err)
+}
+
+func TestBadFrequency(t *testing.T) {
+	o := &options{}
+	o.Options = &bus.Options{}
+	o.Bus = "nop"
+	o.NopMock = "msg_msg_done"
+	o.Rules = append(o.Rules, &Rule{}, &Rule{})
+
+	ws1, _ := newWatchers(o)
+
+	ws1[0].frequency = "bad"
+	err := ws1[0].runWatch()
+	assert.NotNil(t, err)
+}
+
+func TestRunWatch(t *testing.T) {
 	var err error
+	o := &options{}
+	o.Options = &bus.Options{}
+	o.Bus = "nop"
+	o.NopMock = "msg_msg_done"
+	o.Rules = append(o.Rules, &Rule{}, &Rule{})
+
+	ws1, _ := newWatchers(o)
 	go func() {
-		err = run()
+		err = ws1[0].runWatch()
 	}()
+
 	time.Sleep(time.Second)
-	sigChan <- syscall.SIGQUIT
-	if err != nil {
-		t.Fail()
-		t.Errorf("error should not have been returned %v", err)
-	}
-	defer os.Remove("test.toml")
-	defer os.Remove("out.tsks.json") // default tasks out file
-	defer os.Remove("test.file.txt")
-	defer os.Remove(path + "test.file.txt")
-	defer os.Remove("files") // default topic for the files bus
-	defer os.RemoveAll(path[:4])
+	ws1[0].close()
+
+	assert.Nil(t, err)
 }
 
-func createTestWatchFile(path, filename string) {
-	err := os.MkdirAll(path, 0766)
-	if err != nil {
-		panic("dir could not be created " + err.Error())
-	}
-	err = ioutil.WriteFile(path+filename,
-		[]byte(`this is a testing file`), 0666)
-	if err != nil {
-		panic("test toml file could not be created " + err.Error())
-	}
+func TestCurrentFilesErr(t *testing.T) {
+	o := &options{}
+	o.Options = &bus.Options{}
+	o.Bus = "nop"
+	o.NopMock = "msg_msg_done"
+	o.Rules = append(o.Rules, &Rule{}, &Rule{})
+
+	ws1, _ := newWatchers(o)
+	err := ws1[0].currentFiles("nop://err")
+	assert.NotNil(t, err)
 }
 
-func createTomlFile(filename string) {
-	err := ioutil.WriteFile(filename,
-		[]byte(`
-bus = "file" # "" (stdout), "stdout" (default), "file", "nsq"
-[[rule]]
-path_template = "{YYYY}/{MM}/{DD}/{HH}"`), 0666)
-	if err != nil {
-		panic("test toml file could not be created" + err.Error())
-	}
-}
+func TestCompareFileList(t *testing.T) {
+	curCache := make(fileList)
+	curFiles := make(fileList)
 
-func removeContents(dir string) error {
-	d, err := os.Open(dir)
-	if err != nil {
-		return err
+	curFiles["new_file_one"] = &stat.Stats{
+		Size:    123456,
+		Created: "2018-04-12T14:03:53Z",
 	}
-	defer d.Close()
-	names, err := d.Readdirnames(-1)
-	if err != nil {
-		return err
+
+	curFiles["new_file_two"] = &stat.Stats{
+		Size:    321654,
+		Created: "2018-04-12T14:03:53Z",
 	}
-	for _, name := range names {
-		err = os.RemoveAll(filepath.Join(dir, name))
-		if err != nil {
-			return err
-		}
+
+	curFiles["new_file_three"] = &stat.Stats{}
+
+	newFiles := compareFileList(curCache, curFiles)
+
+	// reset for new  cache
+	curCache = curFiles
+	curFiles = make(fileList)
+
+	assert.Equal(t, 2, len(newFiles))
+
+	// date changed should add this as a new file
+	curFiles["new_file_one"] = &stat.Stats{
+		Size:    123456,
+		Created: "2018-04-12T14:44:53Z",
 	}
-	return nil
+
+	// nothing changed, should not add this file as a new file
+	curFiles["new_file_two"] = &stat.Stats{
+		Size:    321654,
+		Created: "2018-04-12T14:03:53Z",
+	}
+
+	// file changed with new size and created, should show as a new file
+	curFiles["new_file_three"] = &stat.Stats{
+		Size:    1234,
+		Created: "2018-04-12T14:44:53Z",
+	}
+
+	// new file should be added in the new file list
+	curFiles["new_file_four"] = &stat.Stats{
+		Created: "2018-04-12T14:41:53Z",
+	}
+
+	newFiles = compareFileList(curCache, curFiles)
+
+	assert.Equal(t, 3, len(newFiles))
+	assert.Equal(t, newFiles["new_file_four"], curFiles["new_file_four"])
 }
