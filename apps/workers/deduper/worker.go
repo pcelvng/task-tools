e78e04094e232a1032d0d4b002179428d6a44833
jbsmith7741 <jbsmith7741@gmail.com>
2018-05-29 14:47:40 -0600

    Reduce number of open readers in deduper worker

--- apps/workers/deduper/worker.go
+++ apps/workers/deduper/worker.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"path/filepath"
 	"sort"
 	"strconv"
@@ -92,24 +91,15 @@ func newWorker(info string) task.Worker {
 	}
 
 	// reader(s)
-	stsRdrs := make(StatsReaders, len(fSts))
-	for i, sts := range fSts {
-		// reader
-		r, err := file.NewReader(sts.Path, fOpt)
-		if err != nil {
-			return task.InvalidWorker(err.Error())
-		}
+	stsFiles := make(StatsFiles, len(fSts))
 
-		// stats reader
-		stsRdrs[i] = &StatsReader{
-			sts:     sts,
-			pthTime: tmpl.PathTime(sts.Path),
-			r:       r,
-		}
+	for i, sts := range fSts {
+		stsFiles[i].Stats = sts
+		stsFiles[i].pthTime = tmpl.PathTime(sts.Path)
 	}
 
 	// sort readers (oldest to newest)
-	sort.Sort(stsRdrs) // implements sort interface
+	sort.Sort(stsFiles) // implements sort interface
 
 	// deduper
 	dedup := dedup.New()
@@ -132,20 +122,22 @@ func newWorker(info string) task.Worker {
 	}
 
 	return &worker{
-		iOpt:    *iOpt,
-		stsRdrs: stsRdrs,
-		dedup:   dedup,
-		w:       w,
+		iOpt:     *iOpt,
+		stsFiles: stsFiles,
+		dedup:    dedup,
+		w:        w,
 
 		indexFields: indexFields,
 	}
 }
 
 type worker struct {
-	iOpt    infoOptions
-	stsRdrs []*StatsReader
-	dedup   *dedup.Dedup
-	w       file.Writer
+	iOpt         infoOptions
+	stsFiles     []StatsFile
+	linesWritten int64
+	files        []string
+	dedup        *dedup.Dedup
+	w            file.Writer
 
 	// csv
 	indexFields []int // csv index fields (set during validation)
@@ -153,26 +145,24 @@ type worker struct {
 
 func (wkr *worker) DoTask(ctx context.Context) (task.Result, string) {
 	// read/write loop
-	for _, rdr := range wkr.stsRdrs { // loop through all readers
-		sts := rdr.sts
-		r := rdr.r
-
-		for {
-			if ctx.Err() != nil {
-				wkr.abort("")
+	for _, rdr := range wkr.stsFiles { // loop through all readers
+		r, err := file.NewReader(rdr.Path, fOpt)
+		if err != nil {
+			return task.Failed(err)
+		}
+		scanner := file.NewScanner(r)
+		for scanner.Scan() {
+			if task.IsDone(ctx) {
+				r.Close()
 				return task.Interrupted()
 			}
 
-			ln, err := r.ReadLine()
-			if err != nil && err != io.EOF {
-				return wkr.abort(fmt.Sprintf("issue at line %v: %v (%v)", r.Stats().LineCnt+1, err.Error(), sts.Path))
-			}
-
-			wkr.addLine(ln)
-
-			if err == io.EOF {
-				break
-			}
+			wkr.addLine(scanner.Bytes())
+		}
+		r.Close()
+		wkr.linesWritten += r.Stats().LineCnt
+		if err := scanner.Err(); err != nil {
+			return task.Failf("issue at line %v: %v (%v)", r.Stats().LineCnt+1, err.Error(), rdr.Path)
 		}
 	}
 
@@ -213,9 +203,6 @@ func (wkr *worker) addLine(ln []byte) {
 // abort will abort processing by closing the
 // reading and then cleaning up written records.
 func (wkr *worker) abort(msg string) (task.Result, string) {
-	for _, rdr := range wkr.stsRdrs {
-		rdr.r.Close()
-	}
 	wkr.w.Abort() // cleanup writes to this point
 
 	return task.ErrResult, msg
@@ -226,10 +213,6 @@ func (wkr *worker) abort(msg string) (task.Result, string) {
 // handle sending created files messages on the
 // producer.
 func (wkr *worker) done() (task.Result, string) {
-	// close
-	for _, rdr := range wkr.stsRdrs {
-		rdr.r.Close()
-	}
 	err := wkr.w.Close()
 	if err != nil {
 		return task.ErrResult, fmt.Sprint(err.Error())
@@ -242,18 +225,9 @@ func (wkr *worker) done() (task.Result, string) {
 	}
 
 	// msg
-	msg := fmt.Sprintf(`read %v lines from %v files and wrote %v lines`, wkr.linesRead(), len(wkr.stsRdrs), wkr.w.Stats().LineCnt)
+	msg := fmt.Sprintf(`read %v lines from %v files and wrote %v lines`, wkr.linesWritten, len(wkr.stsFiles), wkr.w.Stats().LineCnt)
 
 	return task.CompleteResult, msg
-}
-
-// linesRead returns total lines read across all files read.
-func (wkr *worker) linesRead() (lnCnt int64) {
-	for _, rSts := range wkr.stsRdrs {
-		lnCnt += rSts.r.Stats().LineCnt
-	}
-
-	return lnCnt
 }
 
 // parseTmpl is a one-time tmpl parsing that supports the
@@ -274,19 +248,18 @@ func parseTmpl(srcPth, destTmpl string) string {
 	return tmpl.Parse(destTmpl, t)
 }
 
-type StatsReader struct {
-	sts     stat.Stats
+type StatsFile struct {
+	stat.Stats
 	pthTime time.Time // src path extracted time
-	r       file.Reader
 }
 
-type StatsReaders []*StatsReader
+type StatsFiles []StatsFile
 
-func (d StatsReaders) Len() int {
+func (d StatsFiles) Len() int {
 	return len(d)
 }
 
-func (d StatsReaders) Swap(i, j int) {
+func (d StatsFiles) Swap(i, j int) {
 	d[i], d[j] = d[j], d[i]
 }
 
@@ -294,11 +267,11 @@ func (d StatsReaders) Swap(i, j int) {
 // i path time is less than j path time
 // if i and j path times are equal then
 // i created date will check if less then j created.
-func (d StatsReaders) Less(i, j int) bool {
+func (d StatsFiles) Less(i, j int) bool {
 	// first by path src date
 	if d[i].pthTime.Equal(d[j].pthTime) {
 		// then by created date (if path src date is equal)
-		return d[i].sts.ParseCreated().Before(d[j].sts.ParseCreated())
+		return d[i].ParseCreated().Before(d[j].ParseCreated())
 	}
 
 	// is i pthTime before j pthTime?
