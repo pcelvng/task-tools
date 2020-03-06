4ad0ff3409fb95d4e975f7ffdf13db09f73c18a1
jbsmith7741 <jbsmith7741@gmail.com>
2020-03-06 15:29:30 -0700

    Update backloader to support pubsub

--- apps/taskmasters/backloader/main.go
+++ apps/taskmasters/backloader/main.go
@@ -12,7 +12,10 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/jbsmith7741/uri"
 	"github.com/pcelvng/task/bus"
+
+	tools "github.com/pcelvng/task-tools"
 )
 
 var sigChan = make(chan os.Signal, 1)
@@ -59,27 +62,38 @@ func run() error {
 }
 
 var (
-	tskType     = flag.String("type", "", "REQUIRED; the task type")
-	t           = flag.String("t", "", "alias of 'type'")
-	at          = flag.String("at", "", "run once for a specific time. format 'yyyy-mm-ddThh' (example: '2017-01-03T01')")
-	from        = flag.String("from", "now", "format 'yyyy-mm-ddThh' (example: '2017-01-03T01'). Allows a special keyword 'now'.")
-	to          = flag.String("to", "", "same format as 'from'; if not specified, will run the one hour specified by from. Allows special keyword 'now'.")
-	outBus      = flag.String("bus", "stdout", "one of 'stdout', 'file', 'nsq'")
-	b           = flag.String("b", "", "alias of 'bus'")
-	nsqdHosts   = flag.String("nsqd-hosts", "localhost:4150", "comma-separated list of nsqd hosts with port")
+	tskType = flag.String("type", "", "REQUIRED; the task type")
+	at      = flag.String("at", "", "run once for a specific time. format 'yyyy-mm-ddThh' (example: '2017-01-03T01')")
+	from    = flag.String("from", "now", "format 'yyyy-mm-ddThh' (example: '2017-01-03T01'). Allows a special keyword 'now'.")
+	to      = flag.String("to", "", "same format as 'from'; if not specified, will run the one hour specified by from. Allows special keyword 'now'.")
+	outBus  = flag.String("bus", "stdout", "one of 'stdout', 'file', 'nsq', 'pubsub'")
+	//	nsqdHosts   = flag.String("nsqd-hosts", "localhost:4150", "comma-separated list of nsqd hosts with port")
 	template    = flag.String("template", "{yyyy}-{mm}-{dd}T{hh}:00", "task template")
-	topic       = flag.String("topic", "", "overrides task type as the default topic")
+	daily       = flag.Bool("daily", false, "sets hour to 00 and populates every 24 hours")
 	everyXHours = flag.Uint("every-x-hours", 0, "will generate a task every x hours. Includes the first hour. Can be combined with 'on-hours' and 'off-hours' options.")
 	onHours     = flag.String("on-hours", "", "comma separated list of hours to indicate which hours of a day to back-load during a 24 period (each value must be between 0-23). Order doesn't matter. Duplicates don't matter. Example: '0,4,15' - will only generate tasks on hours 0, 4 and 15")
 	offHours    = flag.String("off-hours", "", "comma separated list of hours to indicate which hours of a day to NOT create a task (each value must be between 0-23). Order doesn't matter. Duplicates don't matter. If used will trump 'on-hours' values. Example: '2,9,16' - will generate tasks for all hours except 2, 9 and 16.")
-
-	dFmt = "2006-01-02T15"
+	version     = flag.Bool("version", false, "show version")
+	dFmt        = "2006-01-02T15"
 )
+
+func init() {
+	flag.StringVar(tskType, "t", "", "alias of 'type'")
+	flag.StringVar(outBus, "b", "", "alias of 'bus'")
+	flag.BoolVar(version, "v", false, "show version")
+}
 
 func newOptions() *options {
 	return &options{
 		Options: bus.NewOptions(""),
 	}
+}
+
+type busOptions struct {
+	Bus       string   `uri:"scheme"`
+	Hosts     []string `uri:"host"`
+	ProjectID string   `uri:"project"`
+	JSONAuth  string   `uri:"jsonauth"`
 }
 
 type options struct {
@@ -88,19 +102,12 @@ type options struct {
 	Start time.Time // start of backload
 	End   time.Time // end of backload
 
-	Topic        string // topic override (uses 'TaskType' if not provided)
 	TaskType     string
 	TaskTemplate string
 
 	EveryXHours int    // default skips 0 hours aka does all hours. Will always at least create a task for the start date.
 	OnHours     []bool // each key represents the hour and bool is if that value is turned on. (not specified means all hours are ON)
 	OffHours    []bool // each key represents the hour and bool is if that value is turned off.
-}
-
-// nsqdHostsString will set options.NsqdHosts from a comma
-// separated string of hosts.
-func (c *options) nsqdHostsString(hosts string) {
-	c.NSQdHosts = strings.Split(hosts, ",")
 }
 
 // setOnHours will parse onHours string and set
@@ -196,15 +203,27 @@ func (c *options) validate() error {
 
 func loadOptions() (*options, error) {
 	flag.Parse()
+	if *version {
+		tools.String()
+		os.Exit(0)
+	}
+
+	ops := busOptions{}
+	c := newOptions()
+
+	if err := uri.Unmarshal(*outBus, &ops); err != nil {
+		return nil, err
+	}
+	c.OutBus = ops.Bus
+	c.LookupdHosts = ops.Hosts
+	c.ProjectID = ops.ProjectID
+	c.JSONAuth = ops.JSONAuth
 
 	// load config
-	c := newOptions()
 	c.TaskType = *tskType
 	c.TaskTemplate = *template
 	c.EveryXHours = int(*everyXHours)
-	c.Topic = *topic
-	c.OutBus = *outBus
-	c.nsqdHostsString(*nsqdHosts)
+
 	if err := c.setOnHours(*onHours); err != nil {
 		return nil, err
 	}
@@ -212,16 +231,10 @@ func loadOptions() (*options, error) {
 	if err := c.setOffHours(*offHours); err != nil {
 		return nil, err
 	}
-
-	// alias overrides
-	if *t != "" {
-		c.TaskType = *t
+	if *daily {
+		c.setOnHours("0")
+		c.setOffHours("")
 	}
-
-	if *b != "" {
-		c.OutBus = *b
-	}
-
 	from := *from
 	to := *to
 	if *at != "" {
