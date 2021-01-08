2f58a6091dede502bb2e22656d495081c0914e88
jbsmith7741 <jbsmith7741@gmail.com>
2021-01-08 14:50:57 -0700

    add folder support to glob function

--- file/file_test.go
+++ file/file_test.go
@@ -9,10 +9,9 @@ import (
 	"net/url"
 	"strings"
 
-	"path"
-
 	"fmt"
 
+	"github.com/hydronica/trial"
 	minio "github.com/minio/minio-go"
 	"github.com/pcelvng/task-tools/file/s3"
 )
@@ -22,139 +21,28 @@ var (
 	//
 	// see:
 	// https://docs.minio.io/docs/golang-client-api-reference
-	testEndpoint  = "play.minio.io:9000"
-	testAccessKey = "Q3AM3UQ867SPQQA43P2F"
-	testSecretKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
-	testBucket    = "task-tools-test"
-	testS3Client  *minio.Client
+	testEndpoint = "play.minio.io:9000"
+	testBucket   = "task-tools-test"
+	testS3Client *minio.Client
+
+	wd   string
+	opts = Options{
+		AccessKey: "Q3AM3UQ867SPQQA43P2F",
+		SecretKey: "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+	}
 )
 
-func TestGlob_Local(t *testing.T) {
-	// create files
-	pths := []string{
-		"./test/file-1.txt",
-		"./test/file2.txt",
-		"./test/file3.gz",
-	}
-	for _, pth := range pths {
-		createFile(pth)
-	}
-
-	// test '*'
-	pth := "./test/*.txt"
-	allSts, err := Glob(pth, nil)
-	if err != nil {
-		t.Errorf("expected nil but got '%v'\n", err.Error())
-	}
-
-	if len(allSts) != 2 {
-		t.Errorf("expected %v got %v\n", 2, len(allSts))
-	}
-
-	// test '?'
-	pth = "./test/file?.txt"
-	allSts, err = Glob(pth, nil)
-	if err != nil {
-		t.Errorf("expected nil but got '%v'\n", err.Error())
-	}
-
-	if len(allSts) == 1 {
-		f1 := allSts[0]
-		expected := "file2.txt"
-		_, got := path.Split(f1.Path)
-		if got != expected {
-			t.Errorf("expected file '%v' got '%v'\n", expected, got)
-		}
-	} else {
-		t.Errorf("expected %v got %v\n", 1, len(allSts))
-	}
-
-	// test '?' with '*'
-	pth = "./test/file?.*"
-	allSts, err = Glob(pth, nil)
-	if err != nil {
-		t.Errorf("expected nil but got '%v'\n", err.Error())
-	}
-
-	if len(allSts) != 2 {
-		t.Errorf("expected %v got %v\n", 2, len(allSts))
-	}
-
-	// cleanup
-	for _, pth := range pths {
-		rmFile(pth)
-	}
-	rmFile("./test")
-}
-
-func TestGlob_S3(t *testing.T) {
-	// create files
-	opt := NewOptions()
-	opt.AccessKey = testAccessKey
-	opt.SecretKey = testSecretKey
-	pthDir := fmt.Sprintf("s3://%v/test", testBucket)
-	pths := []string{
-		fmt.Sprintf("%v/file-1.txt", pthDir),
-		fmt.Sprintf("%v/file2.txt", pthDir),
-		fmt.Sprintf("%v/file3.gz", pthDir),
-	}
-	for _, pth := range pths {
-		createFile(pth)
-	}
-
-	// test '*'
-	pth := fmt.Sprintf("%v/*.txt", pthDir)
-	allSts, err := Glob(pth, opt)
-	if err != nil {
-		t.Errorf("expected nil but got '%v'\n", err.Error())
-	}
-
-	if len(allSts) != 2 {
-		t.Errorf("expected %v got %v\n", 2, len(allSts))
-	}
-
-	// test '?'
-	pth = fmt.Sprintf("%v/file?.txt", pthDir)
-	allSts, err = Glob(pth, opt)
-	if err != nil {
-		t.Errorf("expected nil but got '%v'\n", err.Error())
-	}
-
-	if len(allSts) == 1 {
-		f1 := allSts[0]
-		expected := "file2.txt"
-		_, got := path.Split(f1.Path)
-		if got != expected {
-			t.Errorf("expected file '%v' got '%v'\n", expected, got)
-		}
-	} else {
-		t.Errorf("expected %v got %v\n", 1, len(allSts))
-	}
-
-	// test '?' with '*'
-	pth = fmt.Sprintf("%v/file?.*", pthDir)
-	allSts, err = Glob(pth, opt)
-	if err != nil {
-		t.Errorf("expected nil but got '%v'\n", err.Error())
-	}
-
-	if len(allSts) != 2 {
-		t.Errorf("expected %v got %v\n", 2, len(allSts))
-	}
-
-	// cleanup
-	for _, pth := range pths {
-		rmS3File(pth)
-	}
-}
-
 func TestMain(m *testing.M) {
-	// setup
+
+	// setup local files test
+	wd, _ = os.Getwd()
+
+	// setup remote (minio/s3/gcs) test
 	s3.StoreHost = testEndpoint // set test endpoint
 
 	// s3 client
 	var err error
-	testS3Client, err = minio.New(s3.StoreHost, testAccessKey, testSecretKey, true)
+	testS3Client, err = minio.New(s3.StoreHost, opts.AccessKey, opts.SecretKey, true)
 	if err != nil {
 		log.Println(err.Error())
 		os.Exit(1)
@@ -167,45 +55,117 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
+	// create files
+	pths := []string{
+		"test/file-1.txt",
+		"test/file2.txt",
+		"test/file3.gz",
+		"test/f1/file4.gz",
+		"test/f3/file5.txt",
+		"test/f5/file-6.txt",
+	}
+	for _, pth := range pths {
+		createFile("./"+pth, &opts)                                   // local
+		createFile(fmt.Sprintf("s3://%s/%s", testBucket, pth), &opts) // remote
+	}
+
 	code := m.Run()
 
+	// cleanup
+	os.RemoveAll("./test/")
 	rmS3Bucket(testBucket)
 	os.Exit(code)
 }
 
-func createFile(pth string) {
-	opt := NewOptions()
-	opt.SecretKey = testSecretKey
-	opt.AccessKey = testAccessKey
+func TestGlob_Local(t *testing.T) {
+	fn := func(input trial.Input) (interface{}, error) {
+		sts, err := Glob(input.String(), nil)
+		files := make([]string, len(sts))
+		for i := 0; i < len(sts); i++ {
+			files[i] = strings.Replace(sts[i].Path, wd, ".", -1)
+		}
+		return files, err
+	}
+	cases := trial.Cases{
+		"star.txt": {
+			Input:    "./test/*.txt",
+			Expected: []string{"./test/file-1.txt", "./test/file2.txt"},
+		},
+		"file?.txt": {
+			Input:    "./test/file?.txt",
+			Expected: []string{"./test/file2.txt"},
+		},
+		"file?.star": {
+			Input:    "./test/file?.*",
+			Expected: []string{"./test/file2.txt", "./test/file3.gz"},
+		},
+		"folders": {
+			Input:    "./test/*/*",
+			Expected: []string{"./test/f1/file4.gz", "./test/f3/file5.txt", "./test/f5/file-6.txt"},
+		},
+		"range": {
+			Input:    "test/f[1-3]/*",
+			Expected: []string{"./test/f1/file4.gz", "./test/f3/file5.txt"},
+		},
+		"folder/star.txt": {
+			Input:    "test/*/*.txt",
+			Expected: []string{"./test/f3/file5.txt", "./test/f5/file-6.txt"},
+		},
+	}
+	trial.New(fn, cases).SubTest(t)
 
+}
+
+func TestGlob_S3(t *testing.T) {
+	path := "s3://" + testBucket
+	fn := func(input trial.Input) (interface{}, error) {
+		sts, err := Glob(input.String(), &opts)
+		files := make([]string, len(sts))
+		for i := 0; i < len(sts); i++ {
+			files[i] = sts[i].Path
+		}
+		return files, err
+	}
+	cases := trial.Cases{
+		"star.txt": {
+			Input:    path + "/test/*.txt",
+			Expected: []string{path + "/test/file-1.txt", path + "/test/file2.txt"},
+		},
+		"file?.txt": {
+			Input:    path + "/test/file?.txt",
+			Expected: []string{path + "/test/file2.txt"},
+		},
+		"file?.star": {
+			Input:    path + "/test/file?.*",
+			Expected: []string{path + "/test/file2.txt", path + "/test/file3.gz"},
+		},
+		"folders": {
+			Input:    path + "/test/*/*",
+			Expected: []string{path + "/test/f1/file4.gz", path + "/test/f3/file5.txt", path + "/test/f5/file-6.txt"},
+		},
+		"range": {
+			Input:    path + "/test/f[1-3]/*",
+			Expected: []string{path + "/test/f1/file4.gz", path + "/test/f3/file5.txt"},
+		},
+		"folder/star.txt": {
+			Input:    path + "/test/*/*.txt",
+			Expected: []string{path + "/test/f3/file5.txt", path + "/test/f5/file-6.txt"},
+		},
+	}
+	trial.New(fn, cases).SubTest(t)
+}
+
+func createFile(pth string, opt *Options) {
 	w, _ := NewWriter(pth, opt)
 	w.WriteLine([]byte("test line"))
 	w.WriteLine([]byte("test line"))
 	w.Close()
 }
 
-func rmFile(pth string) {
-	fType := parseScheme(pth)
-
-	if fType == "s3" {
-		rmS3File(pth)
-	}
-
-	if fType == "nop" {
-		return
-	}
-
-	os.Remove(pth)
-}
-
 func createBucket(bckt string) error {
 	exists, err := testS3Client.BucketExists(bckt)
-	if err != nil {
+	if err != nil || exists {
 		return err
-	}
-
-	if exists {
-		return nil
 	}
 
 	return testS3Client.MakeBucket(bckt, "us-east-1")
