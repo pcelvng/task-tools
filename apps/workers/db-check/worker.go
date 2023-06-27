1f129c914f7f1dc6b8d115954852214b7e848931
Ryan Mortensen <ryan.mortensen@rakuten.com>
2023-06-27 12:28:47 -0600

    add zero sum check to db-check

--- apps/workers/db-check/worker.go
+++ apps/workers/db-check/worker.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"reflect"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jbsmith7741/uri"
@@ -22,7 +25,18 @@ type worker struct {
 	Type      string    `uri:"type" required:"true"`                     // type of check
 	Field     string    `uri:"field"`                                    // field name being checked
 	DateField string    `uri:"date_field" required:"true"`               // date/time field to query
+	DateType  string    `uri:"date_type"`                                // date type ("dt" = date, "ts" = timestamp)
 	Date      time.Time `uri:"date" format:"2006-01-02" required:"true"` // date value to use in query
+	GroupTS   string    `uri:"group_ts"`                                 // date field to group by
+}
+
+type ZeroRec struct {
+	Field string
+	Hour  int
+}
+
+type MissingRec struct {
+	Hour int
 }
 
 func (o *options) newWorker(info string) task.Worker {
@@ -35,9 +49,14 @@ func (o *options) newWorker(info string) task.Worker {
 	if err := uri.Unmarshal(info, w); err != nil {
 		return task.InvalidWorker("params uri.unmarshal: %v", err)
 	}
-
-	if w.Type == "null" && w.Field == "" {
-		return task.InvalidWorker("field is required on null checks")
+	if (w.Type == "null" || w.Type == "zero") && w.Field == "" {
+		return task.InvalidWorker("field is required on %s checks", w.Type)
+	}
+	if w.Type == "zero" && w.DateType != "dt" && w.DateType != "ts" {
+		return task.InvalidWorker("date_type (dt|ts) is required on zero checks")
+	}
+	if w.Type == "zero" && w.DateType == "ts" && w.GroupTS != "" {
+		return task.InvalidWorker("group_ts only valid with 'dt' date_type")
 	}
 
 	return w
@@ -49,6 +68,8 @@ func (w *worker) DoTask(ctx context.Context) (task.Result, string) {
 		return w.CheckMissing(ctx)
 	case "null":
 		return w.CheckNull(ctx)
+	case "zero":
+		return w.CheckZeroSum(ctx)
 	default:
 		return task.Failf("unsupported type %s", w.Type)
 	}
@@ -133,4 +154,129 @@ func (w *worker) GetNullCount(ctx context.Context) (count int64, err error) {
 		return 0, errors.Wrap(err, "postgres scan")
 	}
 	return count, nil
+}
+
+func (w *worker) CheckZeroSum(ctx context.Context) (task.Result, string) {
+	d := w.Date.Format("2006-01-02")
+	m := w.slack.NewMessage(":radioactive_sign: *task-tools db-check - Zero Sum Check - " + d + "*")
+	issues := 0
+
+	zr, mr, err := w.GetZeroSums(ctx)
+	if err != nil {
+		issues++
+		// there was an error when checking table data add a slack message block with the error
+		m.AddElements(fmt.Sprintf(":octagonal_sign:  *(%s) %s; %s", w.DBSrc, w.Table, err.Error()))
+		log.Printf("(%s) %s; %s\n", w.DBSrc, w.Table, err.Error())
+	} else {
+		if zr != nil {
+			for _, r := range zr {
+				// zero sum found - send a slack message alerting for the table, hour and field
+				issues++
+				m.AddElements(fmt.Sprintf(":no_entry_sign:  *(%s) %s; (Hour: %d) %s* - zero sum", w.DBSrc, w.Table, r.Hour, r.Field))
+				log.Printf("zero sum: (%s) %s; (hour: %d) %s %s\n", w.DBSrc, w.Table, r.Hour, r.Field, d)
+			}
+		}
+		if mr != nil {
+			for _, r := range mr {
+				// missing record(s) - send a slack message alerting for the table & missing hour
+				issues++
+				m.AddElements(fmt.Sprintf(":no_entry_sign:  *(%s) %s; (Hour: %d)* - missing record(s)", w.DBSrc, w.Table, r.Hour))
+				log.Printf("missing record(s): (%s) %s; (hour: %d) %s\n", w.DBSrc, w.Table, r.Hour, d)
+			}
+		}
+	}
+
+	// for any issues, send slack message
+	if issues > 0 {
+		w.slack.SendMessage(m)
+	}
+
+	return task.Completed("table %s; zero sum check completed for date %s, issues: %d", w.Table, d, issues)
+}
+
+func (w *worker) GetZeroSums(ctx context.Context) (zr []ZeroRec, mr []MissingRec, err error) {
+	pg, found := w.Psql[w.DBSrc]
+	if !found {
+		return nil, nil, fmt.Errorf("db source %s not found", w.DBSrc)
+	}
+
+	selString := ""
+	s := strings.Split(strings.Replace(w.Field, " ", "", -1), ",")
+	for _, v := range s {
+		selString += fmt.Sprintf("sum(%s) as %s,", v, v)
+	}
+	selString = selString[:len(selString)-1] //remove last comma
+
+	asDate := ""
+	grpString := ""
+	if w.GroupTS == "" {
+		grpString = fmt.Sprintf("group by %s order by %s", w.DateField, w.DateField)
+		asDate = w.DateField
+	} else {
+		grpString = fmt.Sprintf("group by %s order by %s", w.GroupTS, w.GroupTS)
+		asDate = w.GroupTS
+	}
+	hrCnt := 1
+	if w.DateType == "ts" || w.GroupTS != "" {
+		hrCnt = 24
+	}
+	foundHours := make([]bool, hrCnt)
+
+	qStr := fmt.Sprintf("select %s, %s as date from %s where date(%s) = '%s' %s;", selString, asDate, w.Table, w.DateField, w.Date.Format("2006-01-02"), grpString)
+	rows, err := pg.DB.QueryxContext(ctx, qStr)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "postgres query")
+	}
+	defer rows.Close()
+
+	colNames, err := rows.Columns()
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "postgres columns")
+	}
+	var colVals = make(map[string]interface{})
+	cols := make([]interface{}, len(colNames))
+	colPtrs := make([]interface{}, len(colNames))
+	for i := 0; i < len(colNames); i++ {
+		colPtrs[i] = &cols[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(colPtrs...)
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "postgres scan")
+		}
+		for i, col := range cols {
+			colVals[colNames[i]] = col
+		}
+		hour := 0
+		zeroCols := make([]string, 0)
+		for cName, cVal := range colVals {
+			valType := reflect.TypeOf(cVal)
+			switch vt := valType.String(); vt {
+			case "int64":
+				if cVal.(int64) == 0 {
+					zeroCols = append(zeroCols, cName)
+				}
+			case "[]uint8":
+				v, _ := strconv.ParseFloat(string(cVal.([]uint8)), 64)
+				if v == 0 {
+					zeroCols = append(zeroCols, cName)
+				}
+			case "time.Time":
+				hour = cVal.(time.Time).Hour()
+				foundHours[hour] = true
+			default:
+				return nil, nil, fmt.Errorf("unsupported column type %s", vt)
+			}
+		}
+		for _, cName := range zeroCols {
+			zr = append(zr, ZeroRec{Field: cName, Hour: hour})
+		}
+	}
+
+	for hour, found := range foundHours {
+		if !found {
+			mr = append(mr, MissingRec{Hour: hour})
+		}
+	}
+	return zr, mr, nil
 }
