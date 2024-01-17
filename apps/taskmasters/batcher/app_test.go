b61638815525f3d42f76a3307b7cfd033271191b
Joshua Smith <jbsmith7741@gmail.com>
2024-01-17 16:36:06 -0700

    implement template from file

--- apps/taskmasters/batcher/app_test.go
+++ apps/taskmasters/batcher/app_test.go
@@ -14,7 +14,7 @@ func TestTaskMaster_generate(t *testing.T) {
 		info         string
 		topic        string
 	}
-	fn := func(tOpts testOpts) (any, error) {
+	fn := func(tOpts testOpts) ([]string, error) {
 		p, _ := nop.NewProducer(tOpts.producerMock)
 		tm := &taskMaster{
 			producer: p,
@@ -24,36 +24,31 @@ func TestTaskMaster_generate(t *testing.T) {
 		return p.Messages[tOpts.topic], err
 	}
 
-	trial.New(fn, trial.Cases[testOpts, any]{
+	trial.New(fn, trial.Cases[testOpts, []string]{
 		"batch 2 days with meta": {
 			Input: testOpts{
 				info:  "?task-type=test&meta=job:job_name&from=2021-04-01T00&for=48h&daily#?date={yyyy}-{mm}-{dd}",
 				topic: "test",
 			},
-			Expected: producerResponse(`"meta":"batcher=true&job=job_name"`, `"meta":"batcher=true&job=job_name"`),
+			Expected: []string{`"meta":"batcher=true&job=job_name"`, `"meta":"batcher=true&job=job_name"`},
 		},
-
 		"batch 2 days task": {
 			Input: testOpts{
 				info:  "?task-type=test&from=2021-04-01T00&for=48h&daily#?date={yyyy}-{mm}-{dd}",
 				topic: "test",
 			},
-			Expected: producerResponse(`{"type":"test","info":"?date=2021-04-01"`, `{"type":"test","info":"?date=2021-04-02"`),
+			Expected: []string{`{"type":"test","info":"?date=2021-04-01"`, `{"type":"test","info":"?date=2021-04-02"`},
 		},
 		"batch 2 hours task": {
 			Input: testOpts{
 				info:  "?task-type=test&from=2018-05-10T00&for=2h#?date={yyyy}-{mm}-{dd}T{hh}",
 				topic: "test",
 			},
-			Expected: producerResponse(`{"type":"test","info":"?date=2018-05-10T00"`, `{"type":"test","info":"?date=2018-05-10T01"`, `{"type":"test","info":"?date=2018-05-10T02"`),
+			Expected: []string{`{"type":"test","info":"?date=2018-05-10T00"`, `{"type":"test","info":"?date=2018-05-10T01"`, `{"type":"test","info":"?date=2018-05-10T02"`},
 		},
 		"missing type": {
 			Input:       testOpts{info: "?from=2018-05-10T00&for=2h#?date={yyyy}-{mm}-{dd}T{hh}"},
 			ExpectedErr: errors.New("type is required"),
-		},
-		"missing from": {
-			Input:       testOpts{info: "?task-type=test&for=2h#?date={yyyy}-{mm}-{dd}T{hh}"},
-			ExpectedErr: errors.New("from is required"),
 		},
 		"for or to is required": {
 			Input:       testOpts{info: "?task-type=test&from=2018-05-10T00#?date={yyyy}-{mm}-{dd}T{hh}"},
@@ -64,7 +59,19 @@ func TestTaskMaster_generate(t *testing.T) {
 				info:  "?task-type=test&topic=topic&from=2018-05-10T00&for=2h#?date={yyyy}-{mm}-{dd}T{hh}",
 				topic: "topic",
 			},
-			Expected: producerResponse(`{"type":"test","info":"?date=2018-05-10T00"`, `{"type":"test","info":"?date=2018-05-10T01"`, `{"type":"test","info":"?date=2018-05-10T02"`),
+			Expected: []string{`{"type":"test","info":"?date=2018-05-10T00"`, `{"type":"test","info":"?date=2018-05-10T01"`, `{"type":"test","info":"?date=2018-05-10T02"`},
+		},
+		"file batch": {
+			Input: testOpts{
+				info:  "test/data.json?task-type=test#?president={meta:name}&start={meta:start}&end={meta:end}",
+				topic: "test",
+			},
+			Expected: []string{
+				`?president=george washington&start=1789&end=1797`,
+				`?president=john adams&start=1797&end=1801`,
+				`?president=thomas jefferson&start=1801&end=1809`,
+				`?president=james madison&start=1809&end=1817`,
+			},
 		},
 		"producer failed": {
 			Input: testOpts{
@@ -74,9 +81,5 @@ func TestTaskMaster_generate(t *testing.T) {
 			},
 			ShouldErr: true,
 		},
-	}).EqualFn(trial.Contains).Test(t)
-}
-
-func producerResponse(args ...string) []string {
-	return args
+	}).EqualFn(trial.Contains).SubTest(t)
 }
